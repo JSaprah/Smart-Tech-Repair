04b39a3a529d0b8a49a7917f321842a4d9e96a02
Jasmeen Saprah <j.sapraa@gmail.com>
2024-12-19 16:59:18 +0000

    Add image to the top of the pages

--- book_repair/urls.py
+++ book_repair/urls.py
@@ -4,6 +4,7 @@ from django.urls import path
 
 urlpatterns = [
     path('booking/', views.phones_list, name='booking'),
+
     path('login/', views.login, name='login'),
     path('', views.home, name='home'),
 ]
