30d227673655b4b1bb908b838f2ef8eb30e4ac08
Jasmeen Saprah <j.sapraa@gmail.com>
2024-12-30 23:06:39 +0000

    layout improvements

--- book_repair/forms.py
+++ book_repair/forms.py
@@ -34,4 +34,11 @@ class CustomUserCreationForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ('username', 'email', 'password1', 'password2')
+        fields = (
+            'first_name',
+            'last_name',
+            'username',
+            'email',
+            'password1',
+            'password2'
+            )
